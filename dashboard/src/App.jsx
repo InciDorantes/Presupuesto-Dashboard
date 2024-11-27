aa786f7216acb106e409eb484e6faeecbff47dfb
InciDorantes <incidorantes@gmail.com>
2024-11-26 23:09:41 -0600

    cambios pruebas

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -1,17 +1,13 @@
-import React, { useState } from 'react';
 import './App.css';
-import { DndProvider } from "react-dnd";
-import { HTML5Backend } from "react-dnd-html5-backend";
-import DragDrop from "./components/DragDrop";
+import React, { useState } from 'react';
 
 
 function App() {
   return (
-    <DndProvider backend={HTML5Backend}>
       <div className="App">
-        <DragDrop />
+        <h1>Aqui andamos</h1>
       </div>
-    </DndProvider>
+
   );
 }
 
